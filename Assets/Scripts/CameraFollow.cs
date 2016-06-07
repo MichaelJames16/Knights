31317987406d548ab1fd6141a74acb970c6bb0e9
MichaelJames16 <m.harrelson86@gmail.com>
2016-06-06 21:43:29 -0600

    Some Camera Work
    Just trying to get the camera working. A little more trouble than I
    thought. Still broken.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,29 +4,32 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
+    public GameObject free_move_cylinder;
 
-    private float camHeight = 1.75f;
-    private float camDistance = 2.75f;
-    private float facing;
+    private float camHeight = 1f;
+    private float camDistance = 3f;
+    private float cylinderOffset = 1f;
+    private Vector3 lastPosition;
 
 	// Use this for initialization
 	void Start ()
     {
-        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + camHeight, player.transform.position.z - camDistance);
-        this.transform.rotation = player.transform.rotation;
-
-
+        free_move_cylinder.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + cylinderOffset, player.transform.position.z);
+        transform.position = new Vector3(free_move_cylinder.transform.position.x, free_move_cylinder.transform.position.y + camHeight, free_move_cylinder.transform.position.z - camDistance);
+        transform.rotation = player.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(player.transform);
+        transform.LookAt(free_move_cylinder.transform);
 
         if (Input.GetKeyDown("e"))
         {
             centerCam();
         }
+
+        lastPosition = player.transform.position;
     }
 
     private void centerCam()
@@ -34,5 +37,15 @@ public class CameraFollow : MonoBehaviour {
         this.transform.rotation = player.transform.rotation;
         this.transform.position = -player.transform.forward * camDistance;
         this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + camHeight, player.transform.position.z - camDistance);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Calling this");
+        //not being called, I think it's because the script is attached to the camera and not to the cylinder. Go sharks
+        Vector3 direction = transform.position - lastPosition;
+        Vector3 localDirection = transform.InverseTransformDirection(direction);
+
+        free_move_cylinder.transform.position = transform.position + localDirection;
     }
 }
