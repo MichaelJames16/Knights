7d738ae56e313bdf8de6d6326c3b16aef2eb6a41
MichaelJames16 <m.harrelson86@gmail.com>
2016-06-07 19:01:34 -0600

    Camera Follow
    Camera follows the player now in terms of rotation, not forward and
    backward movement though. Also really jerky and not smooth.

---
+++ Assets/Scripts/player_follow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_follow : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 lastPosition;
+    private float speed = 1f;
+
+    void Update()
+    {
+        //lastPosition = transform.
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+        Debug.Log("Calling this");
+        //not being called, I think it's because the script is attached to the camera and not to the cylinder. Go sharks
+        //Vector3 direction = transform.position - lastPosition;
+        //Vector3 localDirection = transform.InverseTransformDirection(direction);
+
+        //transform.position = transform.position + localDirection;
+    }
+}
